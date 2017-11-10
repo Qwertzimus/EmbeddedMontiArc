c73b5b4866e8b9446b7a03d14689497a4504c595
Manuel Schrick <manuel.schrick@rwth-aachen.de>
2017-11-10 10:36:48 +0100

    added test that covers subcomponents with ports that have the same name as their super component

--- src/test/java/de/monticore/lang/embeddedmontiarc/ExpandedComponentInstanceTest.java
+++ src/test/java/de/monticore/lang/embeddedmontiarc/ExpandedComponentInstanceTest.java
@@ -118,6 +118,34 @@ public class ExpandedComponentInstanceTest extends AbstractSymtabTest {
     }
 
     @Test
+    public void testConnectorCorrectness() {
+        Scope symTab = createSymTab("src/test/resources");
+        ExpandedComponentInstanceSymbol inst = symTab.<ExpandedComponentInstanceSymbol>resolve(
+                "testing.subComponentConnector", ExpandedComponentInstanceSymbol.KIND).orElse(null);
+
+        assertNotNull(inst);
+
+        testConnectorCorrectnessForComponent(inst);
+    }
+
+    private void testConnectorCorrectnessForComponent(ExpandedComponentInstanceSymbol inst) {
+        inst.getConnectors().forEach(connectorSymbol -> {
+            assertNotNull(connectorSymbol.getSourcePort());
+            assertNotNull(connectorSymbol.getTargetPort());
+
+            PortSymbol sourcePort = connectorSymbol.getSourcePort();
+            PortSymbol targetPort = connectorSymbol.getTargetPort();
+
+            System.out.println("source: " + sourcePort.getFullName());
+            System.out.println("target: " + targetPort.getFullName() + "\n");
+
+            assertNotEquals(sourcePort.getFullName(), targetPort.getFullName());
+        });
+
+        inst.getSubComponents().forEach(this::testConnectorCorrectnessForComponent);
+    }
+
+    @Test
     public void testGenericA() {
         Scope symTab = createSymTab("src/test/resources");
         ExpandedComponentInstanceSymbol inst = symTab.<ExpandedComponentInstanceSymbol>resolve(
