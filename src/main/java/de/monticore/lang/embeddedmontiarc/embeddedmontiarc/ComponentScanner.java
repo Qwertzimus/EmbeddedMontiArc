65012a9efe194376437b760c81059ea57905c9ee
Sascha Schneiders <sascha.schneiders@rwth-aachen.de>
2018-03-01 00:13:46 +0100

    only fetch model name and do not build symboltable to avoid long reporting times

--- src/main/java/de/monticore/lang/embeddedmontiarc/embeddedmontiarc/ComponentScanner.java
+++ src/main/java/de/monticore/lang/embeddedmontiarc/embeddedmontiarc/ComponentScanner.java
@@ -1,3 +1,23 @@
+/**
+ *
+ *  ******************************************************************************
+ *  MontiCAR Modeling Family, www.se-rwth.de
+ *  Copyright (c) 2017, Software Engineering Group at RWTH Aachen,
+ *  All rights reserved.
+ *
+ *  This project is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 3.0 of the License, or (at your option) any later version.
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this project. If not, see <http://www.gnu.org/licenses/>.
+ * *******************************************************************************
+ */
 package de.monticore.lang.embeddedmontiarc.embeddedmontiarc;
 
 import de.monticore.lang.embeddedmontiarc.embeddedmontiarc._symboltable.ComponentSymbol;
@@ -34,7 +54,7 @@ public class ComponentScanner {
         this.FILE_ENDING = fileEnding;
     }
 
-    public Set<ComponentSymbol> scan() {
+    public Set<String> scan() {
         ComponentScanner.ComponentLanguageFileVisitor v = new ComponentScanner.ComponentLanguageFileVisitor(this);
         try {
             Files.walkFileTree(basePath, v);
@@ -47,13 +67,13 @@ public class ComponentScanner {
     private static class ComponentLanguageFileVisitor extends SimpleFileVisitor<Path> {
 
         private final ComponentScanner scanner;
-        private final Set<ComponentSymbol> set = new HashSet<>();
+        private final Set<String> set = new HashSet<>();
 
         ComponentLanguageFileVisitor(ComponentScanner scanner) {
             this.scanner = scanner;
         }
 
-        Set<ComponentSymbol> getSet() {
+        Set<String> getSet() {
             return Collections.unmodifiableSet(set);
         }
 
@@ -67,10 +87,11 @@ public class ComponentScanner {
                 return FileVisitResult.CONTINUE;
             }
             Path relativePath = scanner.basePath.relativize(file);
-            String streamModelName = getStreamModelName(relativePath);
-            ComponentSymbol s = scanner.symTab.<ComponentSymbol>resolve(streamModelName, ComponentSymbol.KIND).orElse(null);
-            if (s != null) {
-                set.add(s);
+            String modelName = getModelName(relativePath);
+            //ComponentSymbol s = scanner.symTab.<ComponentSymbol>resolve(streamModelName, ComponentSymbol.KIND).orElse(null);
+
+            if (modelName != null) {
+                set.add(modelName);
             } else {
                 Log.warn("could not resolve stream model defined in file " + f.getAbsolutePath());
             }
@@ -89,7 +110,7 @@ public class ComponentScanner {
             return fName.endsWith(scanner.FILE_ENDING);
         }
 
-        private static String getStreamModelName(Path p) {
+        private static String getModelName(Path p) {
             List<String> parts = new ArrayList<>();
             for (Path dirName : p.getParent()) {
                 parts.add(dirName.toString());
